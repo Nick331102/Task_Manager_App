49684fc763f905e9e9fbf6ce73152d329466bd95
nick naglieri <nick331102@yahoo.com>
2023-02-22 13:17:26 -0500

    task_manager and task classes implemented

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import datetime
+import tkinter as tk
 
 class Task:
     def __init__(self, name, deadline):
@@ -27,6 +28,11 @@ class TaskManager:
     def __init__(self, tasks=None):
         self.tasks = tasks or []
         
+    def display_all_tasks(self):
+        for task in self.tasks:
+            print(task)
+     
+     # sort the overdue tasks and print them   
     def display_overdue_tasks(self):
         today = datetime.date.today()
         overdue_tasks = [task for task in self.tasks if task.deadline < today]
@@ -34,7 +40,27 @@ class TaskManager:
         for task in sorted_tasks:
             print(f"{task.name} ({task.deadline})")
         
-        
+task_manager = TaskManager()
+
+task1 = Task("Finish project", datetime.date(2023, 2, 28))
+task2 = Task("Complete CSS Project", datetime.date(2023, 4, 15))
+task3 = Task("Submit proposal", datetime.date(2022, 5, 10))
+task4 = Task("Update website content", datetime.date(2022, 7, 1))
+task5 = Task("Prepare for presentation", datetime.date(2024, 8, 15))
+
+task_manager.tasks.append(task1)
+task_manager.tasks.append(task2)
+task_manager.tasks.append(task3)
+task_manager.tasks.append(task4)
+task_manager.tasks.append(task5)
+
+task_manager.display_all_tasks()
+
+print()
+
+print("Here is a list of the overdue tasks")
+task_manager.display_overdue_tasks()
+      
     
     
     
