5f6c019f3d3f1149df0acc29f15be039c83da309
nick naglieri <nick331102@yahoo.com>
2023-02-21 21:15:58 -0500

    TaskManager class created

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import datetime
+
 class Task:
     def __init__(self, name, deadline):
         self.name = name 
         self.deadline = deadline
     
+    # return a string representation of the object
     def __str__(self):
         return f"{self.name} ({self.deadline})" 
     
@@ -15,9 +18,27 @@ class Task:
     def delete_task(self):
         del self
     
+    # class method.  Create a new task object without creating an instance
     def add_task(cls, name, deadline):
         new_task = cls(name, deadline)
         return new_task
+    
+class TaskManager:
+    def __init__(self, tasks=None):
+        self.tasks = tasks or []
         
+    def display_overdue_tasks(self):
+        today = datetime.date.today()
+        overdue_tasks = [task for task in self.tasks if task.deadline < today]
+        sorted_tasks = sorted(overdue_tasks, key=lambda task: task.deadline)
+        for task in sorted_tasks:
+            print(f"{task.name} ({task.deadline})")
+        
+        
+    
+    
+    
+        
+    
             
     
